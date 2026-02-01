2b0310bee8f788e3492df3ad909fc76a32b56ab5
maleker86 <109919530+maleker86@users.noreply.github.com>
2026-01-31 23:20:21 -0800

    begin Func lesson; roll dice w/ rounds counter

--- Scripting/Methods.gd
+++ Scripting/Methods.gd
@@ -1,9 +1,20 @@
 @tool
 extends EditorScript
 
+func roll_dice(rolls):
+	for roll in rolls:
+		var roll_true: int = roll + 1
+	
+		var result: int = randi() % 6 + 1
+		print("============")
+		print("roll ", roll_true, " of ", rolls, ": ")
+		print("You rolled a ", result)
+
 func _run() -> void:
-	print("Rolling a dice...")
-	#the remainder is necessary; literally just how the func is
-	var result: int = randi() % 6 + 1
-	print("You rolled a ",result)
+	roll_dice(3)
+	pass
+	#print("Rolling a dice...")
+	##the remainder is necessary; literally just how the func is
+	#var result_ex: int = randi() % 6 + 1
+	#print("You rolled a ",result_ex)
 	
