5d602112848a61f062a8f35b933128f604d9b850
maleker86 <109919530+maleker86@users.noreply.github.com>
2026-02-16 18:23:43 -0800

    Start Class 2.0 lesson

--- BeyondBasics/Runner.gd
+++ BeyondBasics/Runner.gd
@@ -5,6 +5,7 @@ extends EditorScript
 # Called when the script is executed (using File -> Run in Script Editor).
 func _run() -> void:
 	var hawkeye = Character.new(100,"scapel","hawkeye")
+	hawkeye.health -= 10
 	#hawkeye.health = 100
 	#hawkeye.item = "scalpel"
 	#hawkeye.name = "hawkeye"
@@ -13,11 +14,11 @@ func _run() -> void:
 	#var hawk_id = hawkeye.get_instance_id()
 	#print(hawk_id)
 	
-	var trapper = Character.new(50, "clamp", "trapper")
+	#var trapper = Character.new(50, "clamp", "trapper")
 	#trapper.health = 50
 	#trapper.item = "clamp"
 	#trapper.name = "trapper"
-	trapper.print_info()
+	#trapper.print_info()
 	#print(trapper.get_instance_id())
 	
 	#cannot have empty now tht init is set up...
